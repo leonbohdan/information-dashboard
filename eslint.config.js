5c91734f411c17d45e238946646dd060c024af34
leonetsb <leonetsb@supportyourapp.com>
2024-08-08 19:45:43 +0300

    Add eslint, prettier and setting to nuxt.config.ts

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    files: ['src/**/**/**/*.vue'],
+    root: true,
+    env: {
+      browser: true,
+      node: true
+    },
+    extends: ['plugin:nuxt/recommended', 'prettier', 'plugin:prettier/recommended'],
+    plugins: ['prettier'],
+    // add your custom rules here
+    rules: {
+      'nuxt/no-cjs-in-config': 'off',
+      'comma-dangle': ['error', 'always-multiline'],
+      'arrow-parens': ['error', 'as-needed'],
+      semi: ['error', 'never'],
+      'vue/max-attributes-per-line': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: false,
+          // 'parser': 'flow',
+          arrowParens: 'avoid',
+          trailingComma: 'all',
+          vueIndentScriptAndStyle: true
+        }
+      ],
+      'vue/valid-v-slot': [2, { allowModifiers: true }]
+    }
+  }
+]
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '/',
+    head: {
+      titleTemplate: '%s | Dashboard',
+      title: 'Dashboard',
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1'
+        },
+        {
+          charset: 'utf-8'
+        }
+      ],
+      link: [],
+      style: [],
+      script: [],
+      noscript: []
+    }
+  },
+  css: [],
+  debug: true,
+  dev: process.env.APP_ENV === 'development',
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: process.env.APP_ENV === 'development' },
+  plugins: [],
+  modules: [],
+  vite: {},
+  vue: {}
 })
